9e8fce25d94decde4a74490c9b28e20c98e144a0
Nekidsteve <Nekidsteve@Mjolnir.local>
2015-06-08 16:34:01 -0400

    monday1630

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name spideridApp
+ * @description
+ * # spideridApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('spideridApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spideridApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spideridApp
+ */
+angular.module('spideridApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
